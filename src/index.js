326a3c9fb4ac1b4996903152bafaf852142e3b0d
arno1415 <Kyan.hamad@gmail.com>
2024-03-25 23:28:59 -0400

    Changed redirect in index.js for auth0

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ root.render(
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={window.location.origin} >
+      redirectUri={"https://kyan-hamad.github.io/"}>
     <App />
     </Auth0Provider>
   </React.StrictMode>
