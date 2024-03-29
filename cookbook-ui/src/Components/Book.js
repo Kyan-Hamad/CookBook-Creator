cd8d89d2f6dd2d0b99e2c60e52224efa4de10562
arno1415 <Kyan.hamad@gmail.com>
2024-03-29 00:55:28 -0400

    Loading animation changed to book. Shelf has books with react logo image for now.

--- cookbook-ui/src/Components/Book.js
+++ cookbook-ui/src/Components/Book.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import '../Styles/Book.css';
+import BookCoverImage from './logo192.png'; // Replace with your book cover image path
 
 const Book = ({ title }) => {
   return (
     <div className="book">
-      <button className="book-button">{title}</button>
+      <button className="book-button">
+        <img src={BookCoverImage} alt={title} />
+      </button>
     </div>
   );
 };
